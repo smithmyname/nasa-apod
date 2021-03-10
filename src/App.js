5e4850ea56c13841b5b383919581ed709b0b6fce
smithmyname <jasonpfranzke@yahoo.com>
2021-03-09 21:48:10 -0600

    nasa-apod

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./component/Home";
+import NasaPhoto from "./component/NasaPhoto";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Route component={Home} path="/" exact />
+        <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </BrowserRouter>
   );
 }
 
